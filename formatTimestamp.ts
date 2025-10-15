c2426e87b240fe12e8669bde35eb13e84df1e14e
SmartmAAn <smartharshitmaan@gmail.com>
2025-10-16 01:03:16 +0530

    Delete formatTimestamp.ts

--- formatTimestamp.ts
+++
@@ -1,9 +0,0 @@
-export const formatTimestamp = (timestamp: number): string => {
-    if (!timestamp) return '';
-    const date = new Date(timestamp);
-    return date.toLocaleTimeString(navigator.language, {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: true
-    });
-};
